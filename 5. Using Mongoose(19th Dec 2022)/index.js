38badc3fefaa74daa68cd6c03478c8042a653e33
AnshulYADAV <anshulyadav98@gmail.com>
2022-12-19 23:05:16 +0530

    Completed the class

--- 5. Using Mongoose(19th Dec 2022)/index.js
+++ 5. Using Mongoose(19th Dec 2022)/index.js
@@ -6,20 +6,44 @@ mongoose.connect("mongodb://localhost:27017/twitter",
     () => { console.log("Connected To MongoDB") },
     err => { console.log("Error :", err.message) })
 
-let cars = mongoose.model('Cars',
-    mongoose.Schema({
-        brand: String,
-        model: String
-    }))
+let engineSchema = mongoose.Schema({ horsePower: Number, cc: Number })
 
-cars.create({ "brand": "Maruti", "model": "Swift" })
-    .then(data => console.log("Success"))
-    .catch(err => console.log("Error"))
+let carSchema = mongoose.Schema({
+    brand: String,
+    model: {
+        type: String,
+        validate: {
+            validator: s => s.length > 5,
+            message: props => `${props.value} model name is too short`
+        }
+    },
+    engine: engineSchema
+})
+
+let cars = mongoose.model('Cars', carSchema)
+
+cars.create({
+    brand: "Maruti",
+    model: "Altoss",
+}).then(data => console.log(data))
+    .catch(err => console.log(err))
 
 let id = mongoose.Types.ObjectId("63a092b93c0de97a1da59c09")
 
-cars.updateOne({ _id: id },
-    { brand: "Audi2" })
-    .then(console.log)
+let update = cars.updateOne({ _id: id },
+    { brand: "Audi2", $inc: { __v: 1 } })
+
+update.then(console.log)
     .catch(console.log)
 
+const search = async function (id) {
+    try {
+        const myCar = await cars.findOne({ _id: id })
+
+        console.log(myCar)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+search(id).then(() => console.log("found!!"))
