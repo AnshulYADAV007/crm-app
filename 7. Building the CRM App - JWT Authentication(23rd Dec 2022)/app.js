4c5f40632088bef5c3dcfc3d9afed396c25bf969
AnshulYADAV <anshulyadav98@gmail.com>
2022-12-23 22:12:34 +0530

    Created the folder JWT

---
+++ 7. Building the CRM App - JWT Authentication(23rd Dec 2022)/app.js
@@ -0,0 +1,47 @@
+const dbConfig = require("./configs/db.config")
+const mongoose = require("mongoose")
+const authController = require("./controllers/auth.controller")
+const express = require('express')
+const User = require("./models/user.model")
+const app = express()
+const bcrypt = require('bcryptjs')
+async function init() {
+    let user = await User.findOne({ userId: "admin" })
+
+    if (user) {
+        console.log("Admin user already present", user)
+        return
+    }
+
+    try {
+        let user = await User.create({
+            name: "Anshul",
+            userId: "admin",
+            email: "admin@gmail.com",
+            userType: "ADMIN",
+            password: bcrypt.hashSync("Welcome1", 8),
+            userStatus: constants.userStatus.approved
+        })
+        console.log(user)
+    } catch (err) {
+        console.log(err.message)
+    }
+}
+
+mongoose.connect(dbConfig.DB_URL)
+app.use(express.json())
+
+const db = mongoose.connection
+db.on("error", () => console.log("Can't connect to DB"))
+db.once("open", () => {
+    console.log("Connected to Mongo DB")
+    init()
+})
+
+let authRouter = require('./routes/auth.routes')
+const constants = require("./utils/constants")
+authRouter(app)
+
+app.get("/", (req, res) => res.send("Hi"))
+
+app.listen(3000, () => console.log("Listening at localhost:3000"))
