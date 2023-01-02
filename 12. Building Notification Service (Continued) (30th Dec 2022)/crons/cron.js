35af278c56af2a7d262cd3ad14877de9c739233f
AnshulYADAV <anshulyadav98@gmail.com>
2023-01-02 21:42:52 +0530

    Added gmail details

--- 12. Building Notification Service (Continued) (30th Dec 2022)/crons/cron.js
+++ 12. Building Notification Service (Continued) (30th Dec 2022)/crons/cron.js
@@ -3,7 +3,7 @@ const TicketNotificationModel = require("../models/ticketNotification.model")
 const EmailTransporter = require("../notifier/emailService")
 
 
-cron.schedule('*/5 * * * * *', async () => {
+cron.schedule('*/30 * * * * *', async () => {
     const notifications = await TicketNotificationModel.find({
         sentStatus: "UN_SENT"
     })
@@ -12,7 +12,7 @@ cron.schedule('*/5 * * * * *', async () => {
 
     notifications.forEach(notification => {
         const mailData = {
-            from: 'crm.notification.service.1992@gmail.com',
+            from: 'anshulyadav9898@gmail.com',
             to: notification.receipientEmails,
             subject: notification.subject,
             text: notification.content
--- 12. Building Notification Service (Continued) (30th Dec 2022)/notifier/emailService.js
+++ 12. Building Notification Service (Continued) (30th Dec 2022)/notifier/emailService.js
@@ -1,11 +1,10 @@
 const nodemailer = require("nodemailer")
 
 module.exports = nodemailer.createTransport({
-    port: 576,
-    host: "smtp.gmail.com",
+    service: "gmail",
+    debug: true,
     auth: {
-        user: 'crm.notification.service.1992@gmail.com',
-        pass: 'q6jqa6NY6'
-    },
-    secure: false
+        user: 'anshulyadav98@gmail.com',
+        pass: 'amjvdhrrjrhikugd'
+    }
 })
