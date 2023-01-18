b687b18627fe9f5716943b1cc616b652bef77c67
AnshulYADAV <anshulyadav98@gmail.com>
2023-01-18 21:13:29 +0530

    Update app.js

--- 20. Relevel Round 3 & 4 - Practise IV(18th Jan 2023)/app.js
+++ 20. Relevel Round 3 & 4 - Practise IV(18th Jan 2023)/app.js
@@ -1,2 +1,14 @@
 const express = require("express")
 const mongoose = require('mongoose')
+const app = express()
+require('./model/order')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+mongoose.connect("mongodb://localhost:27017/delivery_service",
+    () => { console.log("Connected to Mongoose") })
+
+app.listen(7500, () => {
+    console.log("Server now on port 7500")
+})
