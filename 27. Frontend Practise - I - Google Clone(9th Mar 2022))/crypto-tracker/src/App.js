39a5a09186621079748dd47e4fee220fb52f273a
AnshulYADAV <anshulyadav98@gmail.com>
2023-03-09 21:41:21 +0530

    Created the header for the Crypto-Tracker project

---
+++ 27. Frontend Practise - I - Google Clone(9th Mar 2022))/crypto-tracker/src/App.js
@@ -0,0 +1,28 @@
+import { makeStyles } from '@material-ui/core'
+import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}))
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        {/* <Route path="/" component={Homepage} exact />
+        <Route path="/coins/:id" component={CoinPage} exact /> */}
+      </div>
+    </BrowserRouter>
+  );
+}
+
+
+export default App;
---
+++ 27. Frontend Practise - I - Google Clone(9th Mar 2022))/crypto-tracker/src/components/Header.js
@@ -0,0 +1,73 @@
+import {
+    AppBar,
+    Container,
+    MenuItem,
+    Select,
+    Toolbar,
+    Typography,
+} from "@material-ui/core";
+import {
+    createTheme,
+    makeStyles,
+    ThemeProvider,
+} from "@material-ui/core/styles";
+import { useNavigator } from "react-router-dom";
+import { CryptoState } from "../CryptoContext";
+
+const useStyles = makeStyles((theme) => ({
+    title: {
+        flex: 1,
+        color: "gold",
+        fontFamily: "Montserrat",
+        fontWeight: "bold",
+        cursor: "pointer",
+    },
+}));
+
+const darkTheme = createTheme({
+    palette: {
+        primary: {
+            main: "#fff",
+        },
+        type: "dark",
+    },
+});
+
+function Header() {
+    const classes = useStyles();
+    const { currency, setCurrency } = CryptoState();
+
+    const navigate = useNavigator();
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <AppBar color="transparent" position="static">
+                <Container>
+                    <Toolbar>
+                        <Typography
+                            onClick={() => navigate(`/`)}
+                            variant="h6"
+                            className={classes.title}
+                        >
+                            Crypto Tracker
+                        </Typography>
+                        {/* <Button color="inherit">Login</Button> */}
+                        <Select
+                            variant="outlined"
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={currency}
+                            style={{ width: 100, height: 40, marginLeft: 15 }}
+                            onChange={(e) => setCurrency(e.target.value)}
+                        >
+                            <MenuItem value={"USD"}>USD</MenuItem>
+                            <MenuItem value={"INR"}>INR</MenuItem>
+                        </Select>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </ThemeProvider>
+    );
+}
+
+export default Header;
